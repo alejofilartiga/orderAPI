f6249c574f89afe386ab40e1b591b987716376a5
Alejo <alejofilartiga@gmail.com>
2025-04-25 01:13:53 -0300

    Fix Cors

--- models/server.ts
+++ models/server.ts
@@ -29,14 +29,23 @@ export class Server {
     }
 
     middlewares(): void {
-        this.app.use(cors(corsConfig))
+        this.app.use(cors({
+            origin: "https://campitoshop.vercel.app", // Permitir solicitudes desde este origen específico
+            methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"], // Métodos permitidos
+            allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
+            credentials: true // Permitir envío de cookies si es necesario
+        }));
+
+        // Manejar solicitudes preflight (OPTIONS)
+        this.app.options("*", (req, res) => {
+            res.setHeader("Access-Control-Allow-Origin", "https://campitoshop.vercel.app");
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.sendStatus(204); // Respuesta exitosa para preflight
+        });
+
         this.app.use(express.json());
-        this.app.use((req, res, next) => {
-            res.setHeader('Access-Control-Allow-Origin', 'https://campitoshop.vercel.app');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify allowed methods
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify allowed headers
-            next();
-          });
     }
 
     routes():void{
