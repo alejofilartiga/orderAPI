2f5a7fb76740ba1a8d74e2a44595355ad3643592
Alejo <alejofilartiga@gmail.com>
2025-04-25 01:05:57 -0300

    Fix Cors

--- models/server.ts
+++ models/server.ts
@@ -4,7 +4,7 @@ import { DBConnection } from "../db/config";
 import orderRoutes from "../routes/orders"
 
 const corsConfig = {
-    origin: "*", // Permitir solicitudes desde cualquier origen
+    origin: "https://campitoshop.vercel.app", // Permitir solicitudes desde este origen específico
     credentials: true, // Permitir envío de cookies si es necesario
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"], // Métodos permitidos
     allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
@@ -32,13 +32,16 @@ export class Server {
     middlewares(): void {
         this.app.use(cors(corsConfig));
 
-        // Manejar solicitudes preflight (OPTIONS)
-        this.app.options("*", (req, res) => {
-            res.setHeader("Access-Control-Allow-Origin", "*");
+        // Asegúrate de manejar las solicitudes preflight (OPTIONS)
+        this.app.use((req, res, next) => {
+            res.setHeader("Access-Control-Allow-Origin", "https://campitoshop.vercel.app");
             res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
             res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
             res.setHeader("Access-Control-Allow-Credentials", "true");
-            res.sendStatus(204); // Respuesta exitosa para preflight
+            if (req.method === "OPTIONS") {
+                return res.sendStatus(204); // Respuesta exitosa para preflight
+            }
+            next();
         });
 
         this.app.use(express.json());
