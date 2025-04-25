b4b09433faab2b8fbca0a308f10754a9d40c1a65
Alejo <alejofilartiga@gmail.com>
2025-04-25 12:30:01 -0300

    Fix Mongoose

--- db/config.ts
+++ db/config.ts
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 
-export const DBConnection = async ():Promise<void> =>{
-    try{
-        const dbUrl = process.env.DB_URL
-        if(!dbUrl){
-            throw new Error ("La Url no esta definida en los .ENV")
-        };
-        await mongoose.connect(dbUrl)
-        console.log("Database Online")
+export const DBConnection = async (): Promise<void> => {
+    try {
+        const dbUrl = process.env.DB_URL;
+        if (!dbUrl) {
+            throw new Error("La Url no esta definida en los .ENV");
+        }
+        await mongoose.connect(dbUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database Online");
     } catch (error) {
-        console.log(error)
-        throw new Error ("Error al iniciar la DB")
-    };
+        console.log(error);
+        throw new Error("Error al iniciar la DB");
+    }
 };
